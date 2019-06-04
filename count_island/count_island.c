6814011d1a112c1d77f407e66de295fb3ecc701a
Samuel Karani <samuel.karani@berkeley.edu>
2019-06-03 22:57:21 -0700

    updated level 5

--- count_island/count_island.c
+++ count_island/count_island.c
@@ -39,11 +39,28 @@ int invalid_characters(char *s)
 {
 	while (*s)
 	{
-		if (*s != 'X' || *s != '.')
+		if (!(*s == 'X' || *s == '.' || *s == '\n'))
 			return 1;
 		s++;
 	}
 	return 0;
+}
+
+int fstrjoin(char **a, int l, char b)
+{
+	char *tmp = *a;
+	char *ret = malloc((l + 2) * sizeof(char));
+	int i = 0;
+	while (i < l)
+	{
+		ret[i] = tmp[i];
+		i++;
+	}
+	ret[i++] = b;
+	ret[i] = 0;
+	free(tmp);
+	*a = ret;
+	return l + 1;
 }
 
 void convert(t_node *lst, char *map[])
@@ -57,7 +74,6 @@ void convert(t_node *lst, char *map[])
 		free(lst);
 		lst = next;
 	}
-	map[idx] = NULL;
 }
 
 void count(int h, int w, char *map[h], int i, int j, int idx)
@@ -84,7 +100,7 @@ void count_island(int h, int w, char *map[h])
 		{
 			if (map[i][j] == 'X')
 			{
-				count(h, w, map, i, j, idx);
+				count(h, w, map, i, j, idx++);
 				while (j < w && map[i][j] == 'X')
 					j++;
 				if (j == w)
@@ -97,47 +113,69 @@ void count_island(int h, int w, char *map[h])
 	}
 }
 
+void print_map(char **map, int h, int w)
+{
+	int i = 0;
+	while (i < h)
+		write(1, map[i++], w);
+}
+
+#include <stdio.h>
 int main(int ac, char **av)
 {
 	if (ac == 2)
 	{
-		int fd = open(av[1], O_RDWR);
-		if (fd != -1)
+		int fd;
+		if ((fd = open(av[1], O_RDWR)) != -1)
 		{
-			int size, n, h, w, err, last;
-			size = 1025;
-			char buf[size];
+			int n, h, w, err, len;
+			char buf[1], *line;
 			t_node *lst = NULL;
-			h = err = last = 0;
+			h = err = len = 0;
 			w = -1;
-			while (!err && (n = read(fd, buf, size)))
+			line = NULL;
+			while (!err && (n = read(fd, buf, 1)) > 0)
 			{
-				if (last)
-				{
+				len = fstrjoin(&line, len, *buf);
+				if (invalid_characters(line))
 					err = 1;
-					break ;
-				}
-				buf[n] = 0;
-				add(&lst, buf);
-				if (w == -1)
-					w = n;
-				else if (n != w)
+				if (*buf == '\n')
 				{
-					if (n == w - 1)
-						last = 1;
-					else
+					if (w == -1 && h == 0)
+						w = len;
+					else if (len != w)
 						err = 1;
+					if (!err)
+					{
+						write(0, line, len);
+						add(&lst, line);
+						len = 0;
+						line = NULL;
+						h++;
+					}
 				}
-				if (invalid_characters(buf))
-					err = 1;
-				h++;
 			}
-			if (!err)
+			if (!err && n != -1)
 			{
-				char *map[h + 1];
-				convert(lst, map);
-				count_island(h, w, map);
+				int endline, no_endline;
+				endline = *buf == '\n' && len == w;
+				no_endline = *buf != '\n' && len == w - 1;
+				if (endline || no_endline)
+				{
+					if (no_endline)
+					{
+						add(&lst, line);
+						write(0, line, len);
+						write(1, "\n\n", 2);
+						h++;
+					}
+					char *map[h];
+					convert(lst, map);
+					count_island(h, w, map);
+					print_map(map, h, w);
+				}
 			}
+			close(fd);
 		}
 	}
 	fputstr("\n");
