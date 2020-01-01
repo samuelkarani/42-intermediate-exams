344fd703cc7e78c20b9a39b3daceeb0ad2ef3ec4
Samuel Karani <samuel.karani@berkeley.edu>
2019-12-31 17:43:48 -0800

    bug fix

--- longest_sequence/longest_sequence2.c
+++ longest_sequence/longest_sequence2.c
@@ -1,19 +1,20 @@
 
 struct s_node
 {
-        int value;
-        struct s_node *left;
-        struct s_node *right;
+	int value;
+	struct s_node *left;
+	struct s_node *right;
 };
 
-int helper(struct s_node *node, int ret)
+int helper(struct s_node *node, int n)
 {
+	int ret = n;
 	struct s_node *left = node->left, *right = node->right;
 	int l;
 	if (left)
 	{
 		if (left->value - node->value == 1)
-			l = helper(left, ret + 1);
+			l = helper(left, n + 1);
 		else
 			l = helper(left, 1);
 		ret = l > ret ? l : ret;
@@ -21,7 +22,7 @@ int helper(struct s_node *node, int ret)
 	if (right)
 	{
 		if (right->value - node->value == 1)
-			l = helper(right, ret + 1);
+			l = helper(right, n + 1);
 		else
 			l = helper(right, 1);
 		ret = l > ret ? l : ret;
@@ -29,14 +30,13 @@ int helper(struct s_node *node, int ret)
 	return ret;
 }
 
-int     longest_sequence(struct s_node *node)
+int longest_sequence(struct s_node *node)
 {
 	if (!node)
 		return 0;
 	return helper(node, 1);
 }
 
-/*
 #include <stdio.h>
 #include <stdlib.h>
 typedef struct s_node t_node;
@@ -56,7 +56,7 @@ int main()
 	ten->left->right = cr(9);
 	ten->left->left->left = cr(7);
 	ten->left->left->right = cr(13);
-	printf("%d\n", longest_sequence(ten));	
+	printf("%d\n", longest_sequence(ten));
 
 	t_node *five = cr(5);
 	five->left = cr(6);
@@ -66,8 +66,7 @@ int main()
 	five->right->left->left = cr(3);
 	five->right->left->right = cr(4);
 	five->right->right->right = cr(2);
-	printf("%d\n", longest_sequence(five));	
-
+	printf("%d\n", longest_sequence(five));
 
 	t_node *thirty = cr(30);
 	thirty->left = cr(15);
@@ -77,4 +76,3 @@ int main()
 	printf("%d\n", longest_sequence(thirty));
 	printf("%d\n", longest_sequence(NULL));
 }
-*/
